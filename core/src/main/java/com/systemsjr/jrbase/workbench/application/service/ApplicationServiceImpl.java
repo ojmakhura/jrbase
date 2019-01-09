75414c1256eb742a1f4f57839b5760047162a8fe
Onalenna J. Makhura <ojmakhura@systemsjr.com>
2019-01-09 09:41:06 +0200

    upgraded andromda to 4.0-SNAPSHOT

---
+++ core/src/main/java/com/systemsjr/jrbase/workbench/application/service/ApplicationServiceImpl.java
@@ -0,0 +1,61 @@
+// license-header java merge-point
+/**
+ * This is only generated once! It will never be overwritten.
+ * You can (and have to!) safely modify it by hand.
+ * TEMPLATE:    SpringServiceImpl.vsl in andromda-spring cartridge
+ * MODEL CLASS: AndroMDAModel::jrbase::com.systemsjr.jrbase.workbench::application::service::ApplicationService
+ * STEREOTYPE:  Service
+ */
+package com.systemsjr.jrbase.workbench.application.service;
+
+import com.systemsjr.jrbase.workbench.application.vo.ApplicationSearchCriteria;
+import com.systemsjr.jrbase.workbench.application.vo.ApplicationVO;
+
+/**
+ * @see com.systemsjr.jrbase.workbench.application.service.ApplicationService
+ */
+public class ApplicationServiceImpl
+    extends ApplicationServiceBase
+{
+
+    /**
+     * @see com.systemsjr.jrbase.workbench.application.service.ApplicationService#loadAllApplications()
+     */
+    protected  ApplicationVO[] handleLoadAllApplications()
+        throws Exception
+    {
+        // TODO implement protected  ApplicationVO[] handleLoadAllApplications()
+        throw new UnsupportedOperationException("com.systemsjr.jrbase.workbench.application.service.ApplicationService.handleLoadAllApplications() Not implemented!");
+    }
+
+    /**
+     * @see com.systemsjr.jrbase.workbench.application.service.ApplicationService#saveApplication(ApplicationVO)
+     */
+    protected  ApplicationVO handleSaveApplication(ApplicationVO applicationVO)
+        throws Exception
+    {
+        // TODO implement protected  ApplicationVO handleSaveApplication(ApplicationVO applicationVO)
+        throw new UnsupportedOperationException("com.systemsjr.jrbase.workbench.application.service.ApplicationService.handleSaveApplication(ApplicationVO applicationVO) Not implemented!");
+    }
+
+    /**
+     * @see com.systemsjr.jrbase.workbench.application.service.ApplicationService#removeApplication(ApplicationVO)
+     */
+    protected  void handleRemoveApplication(ApplicationVO applicationVO)
+        throws Exception
+    {
+        // TODO implement protected  void handleRemoveApplication(ApplicationVO applicationVO)
+        throw new UnsupportedOperationException("com.systemsjr.jrbase.workbench.application.service.ApplicationService.handleRemoveApplication(ApplicationVO applicationVO) Not implemented!");
+    }
+
+    /**
+     * @see com.systemsjr.jrbase.workbench.application.service.ApplicationService#searchApplication(ApplicationSearchCriteria)
+     */
+    protected  ApplicationVO[] handleSearchApplication(ApplicationSearchCriteria searchCriteria)
+        throws Exception
+    {
+        // TODO implement protected  ApplicationVO[] handleSearchApplication(ApplicationSearchCriteria searchCriteria)
+        throw new UnsupportedOperationException("com.systemsjr.jrbase.workbench.application.service.ApplicationService.handleSearchApplication(ApplicationSearchCriteria searchCriteria) Not implemented!");
+    }
+
+}
